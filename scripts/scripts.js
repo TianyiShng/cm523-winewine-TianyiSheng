3acd3554704a3bc8ab25fd3661d3be598e7ee43d
Tianyi Sheng <tysheng@bu.edu>
2023-11-28 13:32:03 -0500

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -50,3 +50,60 @@ function showQuestion(questionId) {
   document.getElementById(questionId).style.display = 'flex';
 }
 
+
+
+function selectOption(selectedButton, selectedOption, questionNumber) {
+  const correctAnswers = {
+      'question1': 'A',
+      'question2': 'C', 
+      'question3': 'B', 
+      'question4': 'C', 
+      'question5': 'D'
+  };
+
+  const correctOption = correctAnswers[questionNumber];
+
+  document.querySelectorAll('#' + questionNumber + ' .option').forEach(button => {
+      button.classList.remove('correct', 'incorrect');
+  });
+
+  if (selectedOption === correctOption) {
+      selectedButton.classList.add('correct');
+  } else {
+      selectedButton.classList.add('incorrect');
+      document.getElementById('overlayQuiz-' + questionNumber).style.display = 'block';
+      document.getElementById('feedback-' + questionNumber).style.display = 'block';
+  }
+}
+
+function closeFeedback(questionNumber) {
+  document.getElementById('overlayQuiz-' + questionNumber).style.display = 'none';
+  document.getElementById('feedback-' + questionNumber).style.display = 'none';
+}
+
+function checkAnswersAndOpenPage() {
+  const correctAnswers = {
+      'question1': 'A',
+      'question2': 'C', 
+      'question3': 'B', 
+      'question4': 'C', 
+      'question5': 'D'
+  };
+
+  let allCorrect = true;
+  for (var question in correctAnswers) {
+      const selectedOption = document.querySelector('#' + question + ' .option.correct');
+      if (!selectedOption || selectedOption.textContent.trim().charAt(0) !== correctAnswers[question]) {
+          allCorrect = false;
+          break;
+      }
+  }
+
+  if (allCorrect) {
+      //window.open('completion.html'); 
+      window.location.href = 'completion.html';
+  } else {
+      alert("Some answers are incorrect, please check your answers.");
+  }
+}
+
